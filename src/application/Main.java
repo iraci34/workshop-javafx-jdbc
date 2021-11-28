c12b4105c6f3ce697e58ea054fec74a3054c0582
iraci34 <netinho.iraci@gmail.com>
2021-11-28 10:53:27 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,9 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) { 
 		try {
@@ -19,11 +22,20 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane); primaryStage.setScene(mainScene); 
+			mainScene = new Scene(scrollPane); 
+			primaryStage.setScene(mainScene); 
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 	    } catch (IOException e) {
 	    	e.printStackTrace();
 	    }
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
+	public static void mai(String[] args) {
+		launch(args);
 	}
  }
